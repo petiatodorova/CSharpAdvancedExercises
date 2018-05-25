09305eb701b1ff935720138fa61259017a255d1c
Petya Todorova <invisioncl@gmail.com>
2018-05-25 17:30:05 +0300

    FilesAndDirectories.sln

---
+++ FilesAndDirectories/MyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        public static int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ FilesAndDirectories/Startup/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using MyLibrary;
+
+
+namespace Startup
+{
+    class Startup
+    {
+        static void Main()
+        {
+
+            Directory.CreateDirectory("Test");
+            //Directory.Delete("../../Test", true);
+
+
+
+
+        }
+    }
+}
