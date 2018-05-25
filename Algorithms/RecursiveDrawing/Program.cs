f7c718b55323db9ef4e2ce4fbe5c41f0766ec94b
Petya Todorova <invisioncl@gmail.com>
2018-05-25 17:27:58 +0300

    Algorithms.sln added

---
+++ Algorithms/RecursiveDrawing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RecursiveDrawing
+{
+    class RecursiveDrawing
+    {
+        static void Main(string[] args)
+        {
+            PrintFigure(5);
+        }
+
+        static void PrintFigure(int n)
+
+        {
+
+            if (n == 0) // Bottom of the recursion
+
+                return;
+
+
+
+            // Pre-action: print n asterisks
+
+            Console.WriteLine(new string('*', n));
+
+
+
+            // Recursive call: print figure of size n-1
+
+            PrintFigure(n - 1);
+
+
+
+            // Post-action: print n hashtags
+
+            Console.WriteLine(new string('#', n));
+
+        }
+
+    }
+}
---
+++ Algorithms/Test/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            PrintTriangle(n);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            // Printing the upper part of the triangle
+            for (int line = 1; line <= n; line++)
+            {
+                PrintLine(1, line);
+            }
+            // Printing the bottom part of the triangle
+            // that is under the longest line
+            for (int line = n - 1; line >= 1; line--)
+            {
+                PrintLine(1, line);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
