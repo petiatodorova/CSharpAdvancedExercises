f611950b2026250ab2e89898430a2fe4c03d58cc
Petya Todorova <invisioncl@gmail.com>
2018-09-26 17:29:35 +0300

    StreamReaderAndWriter.csproj

---
+++ FilesAndDirectories/StreamReaderAndWriter/FileReadWrite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace StreamReaderAndWriter
+{
+    class FileReadWrite
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("(full path)\\TextFileToRead.txt"))
+            {
+                using (var streamWrite = new StreamWriter("(full path)\\TextFileToWrite.txt"))
+                {
+                    string line = stream.ReadLine();
+                    while (line != null)
+                    {
+                        for (int i = line.Length - 1; i >= 0; i--)
+                        {
+                            streamWrite.Write(line[i]);
+                        }
+
+                        streamWrite.WriteLine();
+                        line = stream.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
