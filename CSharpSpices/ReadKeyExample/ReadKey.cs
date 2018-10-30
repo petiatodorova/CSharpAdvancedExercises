cbe2439384d787761d34beac6065c5efcb43397e
Petya Todorova <invisioncl@gmail.com>
2018-10-30 12:03:38 +0200

    CSharpSpices folder added.

---
+++ CSharpSpices/ReadKeyExample/ReadKey.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReadKeyExample
+{
+    class ReadKey
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Press 'a' or 'b'. Everything else finishes the program.");
+            
+            ConsoleKeyInfo info = Console.ReadKey();
+
+            while (true)
+            {
+                if (info.KeyChar != 'a' && info.KeyChar != 'b' )
+                {
+                    Console.WriteLine($"");
+                    Console.WriteLine($"You typed {info.Key} so the program is finished.");
+                    Console.WriteLine($"The hash code is {info.GetHashCode()}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"");
+                    Console.WriteLine($"You typed {info.Key}.");
+                    Console.WriteLine($"The hash code is {info.GetHashCode()}");
+                }
+
+                info = Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CSharpSpices/TypeGetType/TypeGetTypeExamp.cs
@@ -0,0 +1,23 @@
+﻿namespace TypeGetType
+{
+    using System;
+
+    class TypeGetTypeExamp
+    {
+        public static void Main()
+        {
+            object[] values = { (int)100, (long)17111 };
+            foreach (var value in values)
+            {
+
+                Type tp = value.GetType();
+
+                if (tp.Equals(typeof(int)))
+                    Console.WriteLine("{0} is an integer data type.", value);
+
+                else
+                    Console.WriteLine("'{0}' is not an int data type.", value);
+            }
+        }
+    }
+}
